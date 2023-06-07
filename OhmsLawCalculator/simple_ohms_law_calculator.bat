95c344db316491fd0a7bbc4bf60fda0746c01d27
stweedo <108593831+stweedo@users.noreply.github.com>
2023-06-07 08:17:53 -0500

    Update simple_ohms_law_calculator.bat

--- OhmsLawCalculator/simple_ohms_law_calculator.bat
+++ OhmsLawCalculator/simple_ohms_law_calculator.bat
@@ -1,2 +1,2 @@
 @echo off
-python -c "import urllib.request; exec(urllib.request.urlopen('https://github.com/stweedo/ElectricalTools/raw/main/OhmsLawCalculator/simple_ohms_law_calculator.py').read())"
+python3 -c "import urllib.request; exec(urllib.request.urlopen('https://github.com/stweedo/ElectricalTools/raw/main/OhmsLawCalculator/simple_ohms_law_calculator.py').read())"
