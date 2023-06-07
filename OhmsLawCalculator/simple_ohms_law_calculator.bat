b92354291b38f838b626a21a042906c30b5d2f48
stweedo <108593831+stweedo@users.noreply.github.com>
2023-06-07 17:05:21 -0500

    Delete simple_ohms_law_calculator.bat

--- OhmsLawCalculator/simple_ohms_law_calculator.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-python3 -c "import urllib.request; exec(urllib.request.urlopen('https://github.com/stweedo/ElectricalTools/raw/main/OhmsLawCalculator/simple_ohms_law_calculator.py').read())"
