e2cc7ccdc3dc0e396160048ef7980148e76e781a
stweedo <108593831+stweedo@users.noreply.github.com>
2023-06-07 01:42:01 -0500

    Add files via upload

---
+++ OhmsLawCalculator/simple_ohms_law_calculator.bat
@@ -0,0 +1,2 @@
+@echo off
+python -c "import urllib.request; exec(urllib.request.urlopen('https://github.com/stweedo/python/raw/main/ElectricalTools/OhmsLawCalculator/simple_ohms_law_calculator.py').read())"
